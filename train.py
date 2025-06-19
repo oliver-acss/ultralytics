419cffe754a87cdb8f81c6cd5f7065bb6f7e5e3a
Yufan Li <53818287+oliver-acss@users.noreply.github.com>
2025-06-20 03:07:24 +0800

    为p2和普通版添加注意力模块

--- train.py
+++ train.py
@@ -1,18 +1,19 @@
 from ultralytics import YOLO
 import os
+
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 if __name__ == '__main__':
     # 使用自定义的yolov8-package模型配置
     model = YOLO('ultralytics/cfg/models/v8/yolov8-package2.yaml')
-    
+
     # 训练参数设置
     results = model.train(
         data="package-seg/package-seg.yaml",  # 数据集配置文件路径
-        epochs=50,                            # 训练轮数
-        imgsz=640,                           # 输入图像大小
-        batch=8                              # 批次大小
+        epochs=50,  # 训练轮数
+        imgsz=640,  # 输入图像大小
+        batch=8  # 批次大小
     )
-    
+
     # 验证模型
     model.val()
